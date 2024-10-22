a3da1924af3ac04036ed7f51019cdb78ea4067e4
CatsCanCode <108442417+lielbr0n@users.noreply.github.com>
2024-10-22 09:46:03 +0800

    remove text limit title

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -35,7 +35,8 @@ class PostRequest extends FormRequest
         }
         
         return [
-            'post_title' => ['required','max:150'], 
+           // 'post_title' => ['required','max:150'], 
+            'post_title' => ['required'],  //set to no limit 255 
             'post_slug' =>  $post_slug_validation,
             'post_content' => ['required'],
             'post_category' => ['required', 'max:50'],
