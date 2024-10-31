8dc86b90677ed5cc1ff2513a36ca4ab5064e40da
lielbr0n <14mmil3x@gmail.com>
2024-10-31 12:13:47 +0800

    update gitingore ang my account link

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -252,7 +252,7 @@
                 <li>
                 <a
                     class="block w-full whitespace-nowrap bg-white px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-[#2C8D0A] hover:text-[#ffffff] focus:bg-zinc-200/60 focus:outline-none active:bg-zinc-200/60 active:no-underline dark:bg-surface-dark dark:text-white dark:hover:bg-neutral-800/25 dark:focus:bg-neutral-800/25 dark:active:bg-neutral-800/25"
-                    href="https://buseco.coop/my-account-login"
+                    href="https://test.buseco.coop/my-account-login"
                     data-twe-dropdown-item-ref
                     >MY ACCOUNT</a>
                 </li>
@@ -482,7 +482,7 @@
                         >
                             <a href="https://online.buseco.coop/" class="block rounded-lg py-1 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:text-[#ffffff] hover:bg-[#2C8D0A]">MEMBERSHIP APPLICATION</a>
                             <a href="https://reconnect.buseco.coop/" class="block rounded-lg py-1 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:text-[#ffffff] hover:bg-[#2C8D0A]">RECONNECTION APPLICATION</a>
-                            <a href="https://buseco.coop/my-account-login/" class="block rounded-lg py-1 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:text-[#ffffff] hover:bg-[#2C8D0A]">MY ACCOUNT</a>
+                            <a href="https://test.buseco.coop/my-account-login/" class="block rounded-lg py-1 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:text-[#ffffff] hover:bg-[#2C8D0A]">MY ACCOUNT</a>
                             <a href="/download-forms" class="block rounded-lg py-1 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:text-[#ffffff] hover:bg-[#2C8D0A]">DOWNLOADABLE FORMS</a>
                             <a href="/faq" class="block rounded-lg py-1 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:text-[#ffffff] hover:bg-[#2C8D0A]">FAQs</a>
                         </div> 
